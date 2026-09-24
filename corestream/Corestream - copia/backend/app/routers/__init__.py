e1c6645f8312e5d36667457cd767c5fdb6d7d1d9
TrophicBus89 <134553018+TrophicBus89@users.noreply.github.com>
2026-09-24 16:24:10 -0300

    fix/eliminar-proyecto-duplicado-2
    Borrando proyecto nombreado "Corestream -copia"

--- corestream/Corestream - copia/backend/app/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Routers del módulo de CoreStream.
-
-Este paquete contiene todos los routers para los endpoints REST de CoreStream.
-"""
--- corestream/Corestream - copia/backend/app/services/__init__.py
+++
@@ -1,1 +0,0 @@
-# Este archivo convierte el directorio 'services' en un paquete de Python.
